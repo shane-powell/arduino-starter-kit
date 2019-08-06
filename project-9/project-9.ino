b532f5bf13ab04d15f419c08a6a292c8c4e9de96
kenjara <kenjara@hotmail.com>
2019-08-06 21:43:08 +0100

    Added Project 9 code

---
+++ project-9/project-9.ino
@@ -0,0 +1,26 @@
+const int switchPin = 2;
+const int motorPin = 9;
+int switchState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(switchPin);
+  digitalWrite(motorPin, switchState);
+
+  
+//Serial.println(switchState);
+//  if (switchState == HIGH) {
+//    digitalWrite(motorPin, HIGH);
+//    Serial.print("HIGH");
+//  } else {
+//    digitalWrite(motorPin, LOW);
+//  }
+}
